2eb0aca86a5424ab440091c1cc5c676bb5207aba
amit <amit1schneider@gmail.com>
2024-10-10 10:59:48 +0300

    with dialog

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myfirstapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,50 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private int counter;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        counter = 10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.e(TAG, "MainActivity::OnCreate");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void knock(View view) {
+        if(counter > 0) {
+            counter--;
+            TextView counterTextView = findViewById(R.id.textview_counter);
+            counterTextView.setText(""+counter);
+            if(counter==0) {
+                ImageView eggImageView = findViewById(R.id.imageview_egg);
+                eggImageView.setImageResource(R.drawable.surprise_egg);
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void reset(View view) {
+        counter=10;
+        TextView counterTextView = findViewById(R.id.textview_counter);
+        counterTextView.setText(""+counter);
+        ImageView eggImageView = findViewById(R.id.imageview_egg);
+        eggImageView.setImageResource(R.drawable.green_egg);
     }
 }
